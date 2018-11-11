c2f8be57d5ecf72fe53daba63c38773a70322e9c
Lisa Kim <lisakeem@outlook.com>
2018-11-11 09:35:19 -0800

    finish main/service.go initializing

--- service/service.go
+++ service/service.go
@@ -11,7 +11,53 @@ import (
 type Service struct{}
 
 func (s *Service) GetStatus (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
-	log.Println(logtag.Info, "Requesting GetStatus service")
 	//TODO
+	logRequestService("GetStatus")
 	return nil, nil
+}
+
+func (s *Service) CreateUser (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	// TODO
+	logRequestService("CreateUser")
+	return nil, nil
+}
+
+func (s *Service) DeleteUser (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("DeleteUser")
+	return nil, nil
+}
+
+func (s *Service) UpdateUser (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("UpdateUser")
+	return nil, nil
+}
+
+func (s *Service) AuthenticateUser (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("AuthenticateUser")
+	return nil, nil
+}
+
+func (s *Service) ListUsers (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("ListUsers")
+	return nil, nil
+}
+
+func (s *Service) GetUser (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("GetUser")
+	return nil, nil
+}
+
+func (s *Service) ShareDocument (ctx context.Context, req *pb.UserRequest) (*pb.UserResponse, error) {
+	//TODO
+	logRequestService("ShareDocument")
+	return nil, nil
+}
+
+func logRequestService (svc string) {
+	log.Println(logtag.Info, "Requesting", svc, "service")
 }
